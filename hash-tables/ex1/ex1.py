21340df72c9cb7d364336862909b18794a66e0a0
Blake Fletcher <blkfltchr@gmail.com>
2018-10-12 13:51:13 -0400

    Hash tables ex1 and ex2 completed

--- hash-tables/ex1/ex1.py
+++ hash-tables/ex1/ex1.py
@@ -1,6 +1,16 @@
 def get_indices_of_item_weights(weights, limit):
-  pass 
+
+  hash_table = { i : weights[i] for i in range(0, len(weights) ) } 
+  hash_table2 = {y:x for x,y in hash_table.items()}
+
+  for weight in hash_table2:
+    try:
+      twoWeights = hash_table2[limit-weight]
+      return (hash_table2[limit-weight], hash_table2[weight])
+    except KeyError: 
+      print('Try again...')
 
 if __name__ == '__main__':
   # You can write code here to test your implementation using the Python repl
-  pass 
+  print(get_indices_of_item_weights([4,6,10,15,16], 21))
+ 
--- hash-tables/ex2/ex2.py
+++ hash-tables/ex2/ex2.py
@@ -1,6 +1,32 @@
 def reconstruct_trip(tickets):
-  pass 
+
+  hash_table = {}
+  
+  route = []
+  
+  for ticket in tickets:
+    hash_table[ticket[0]] = ticket[1]
+  
+  current = hash_table[None]
+  
+  while current is not None:
+    route.append(current)
+    if current in hash_table:
+      current = hash_table[current]
+  return route
 
 if __name__ == '__main__':
   # You can write code here to test your implementation using the Python repl
-  pass
+  tickets = [
+    ('PIT', 'ORD'),
+    ('XNA', 'CID'),
+    ('SFO', 'BHM'),
+    ('FLG', 'XNA'),
+    (None, 'LAX'), 
+    ('LAX', 'SFO'),
+    ('CID', 'SLC'),
+    ('ORD', None),
+    ('SLC', 'PIT'),
+    ('BHM', 'FLG'),
+  ]
+  print(reconstruct_trip(tickets))
